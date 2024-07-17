54085b7dc8103f9f375ee90254450735320aeebf
Big Money? <cole.waymau@gmail.com>
2024-07-17 19:05:12 -0400

    intial commit

---
+++ env_vars.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+os.environ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Q2/Desktop/project/day-71-starting-files-blog-for-deployment/instance/posts.db'
